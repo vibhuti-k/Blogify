dfe6dbd7ae01301b7dece30248dcd1f63b1bf46e
vibhuti <vibhutikumar30062002@gmail.com>
2025-09-18 19:58:46 +0530

    18-09-2025 20:00 New Commit

--- models/user.js
+++ models/user.js
@@ -14,7 +14,6 @@ const userSchema = new Schema({
     },
     salt: {
         type: String,
-        required: true,
     },
     password: {
         type: String,
@@ -45,6 +44,20 @@ userSchema.pre("save", function (next) {
     this.password = hashedPassword;
 
     next();
+});
+
+userSchema.static("matchPassword", async function(email, password) {
+    const user = await this.findOne({ email });
+    if(!user) throw new Error("User not found!");
+
+    const salt = user.salt;
+    const hashedPassword = user.password;
+
+    const userProvidedHash = createHmac("sha256", salt).update(password).digest("hex");
+
+    if(hashedPassword !== userProvidedHash) throw new Error("Incorrect Password");
+
+    return user;
 });
 
 const User = model("user", userSchema);
