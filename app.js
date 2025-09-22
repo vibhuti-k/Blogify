dab8233ebb56c427c78be323daaeaf2153270abf
vibhuti <vibhutikumar30062002@gmail.com>
2025-09-22 20:35:14 +0530

    22-09-2025 20:35 New Commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
@@ -11,9 +13,9 @@ const blogRoute = require("./routes/blog");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-mongoose.connect("mongodb://localhost:27017/blogify").then(e => console.log("MongoDB Connected"));
+mongoose.connect(process.env.MONGO_URL).then(e => console.log("MongoDB Connected"));
 
 app.set("view engine","ejs");
 app.set("views", path.resolve("./views"));
